0f4e105ae743c23b1b007d5022712a7a39acff8f
kevin cando <scando@fiec.espol.edu.ec>
2016-08-14 17:47:56 -0500

    Estado inicial

---
+++ build/Debug/GNU-Linux/main.o.d
@@ -0,0 +1,28 @@
+build/Debug/GNU-Linux/main.o: main.c ../../../libDEA/include/graph.h \
+ ../../../libDEA/include/nodelist.h ../../../libDEA/include/generic.h \
+ ../../../libDEA/include/Const.h ../../../libDEA/include/ErrorHandler.h \
+ ../../../libDEA/include/debugSys.h ../../../libDEA/include/Exec.h \
+ ../../../libDEA/include/MemHandlers.h ../../../libDEA/include/list.h \
+ ../../../libDEA/include/stack.h ../../../libDEA/include/queue.h
+
+../../../libDEA/include/graph.h:
+
+../../../libDEA/include/nodelist.h:
+
+../../../libDEA/include/generic.h:
+
+../../../libDEA/include/Const.h:
+
+../../../libDEA/include/ErrorHandler.h:
+
+../../../libDEA/include/debugSys.h:
+
+../../../libDEA/include/Exec.h:
+
+../../../libDEA/include/MemHandlers.h:
+
+../../../libDEA/include/list.h:
+
+../../../libDEA/include/stack.h:
+
+../../../libDEA/include/queue.h:
---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: kevincando
+ *
+ * Created on August 14, 2016, 5:45 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "graph.h"
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
